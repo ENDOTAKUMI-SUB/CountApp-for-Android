8701aad2e2966b009f58a9fb758cbc6f1c8dc5c9
遠藤拓巳 <en0605taku@gmail.com>
2019-05-26 00:55:54 +0900

    Create code

--- app/src/main/java/com/endo_takumi/mvp_original_001/MainActivity.kt
+++ app/src/main/java/com/endo_takumi/mvp_original_001/MainActivity.kt
@@ -2,11 +2,32 @@ package com.endo_takumi.mvp_original_001
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), MainContract.View {
+    // Definition
+    private lateinit var presenter: MainPresenter
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        // Create the presenter
+        presenter = MainPresenter(this, this)
+
+        // When the add button is pressed
+        addButton.setOnClickListener {
+            presenter.add()
+        }
+
+        // When the remove button is pressed
+        removeButton.setOnClickListener {
+            presenter.remove()
+        }
+    }
+
+    // Update the screen
+    override fun redraw(count: Int) {
+        countTextView.setText(count.toString())
     }
 }
--- app/src/main/java/com/endo_takumi/mvp_original_001/MainContract.kt
+++ app/src/main/java/com/endo_takumi/mvp_original_001/MainContract.kt
@@ -1,2 +1,14 @@
 package com.endo_takumi.mvp_original_001
 
+interface MainContract {
+    // General operation of View
+    interface View {
+        fun redraw(count:Int)
+    }
+
+    // Other than that
+    interface Presenter {
+        fun add()
+        fun remove()
+    }
+}
